806f9de5c7d7f36b3aaae84c5d3dd307406a6bea
jupi-z <ameno.aton4@gmail.com>
2024-01-07 11:57:17 +0200

    j'ai tout recommencé de zéro avec un nouveau template, vuexy

--- app_orientation/views.py
+++ app_orientation/views.py
@@ -4,35 +4,34 @@ from .forms import QuestionnaireForm
 from .forms import VotreFormulaire
 
 
-
 def accueil(request):
-    page_active = 'accueil'
-    return render(request, 'accueil.html', {'page_active': page_active})
+    page_active = '/'
+    return render(request, 'index.html', {'page_active': page_active})
 
 
 def about(request):
     page_active = 'about'
-    return render(request, 'about.html', {'page_active': page_active})
+    return render(request, "pages/about.html", {'page_active': page_active})
 
 
 def contact(request):
     page_active = 'contact'
-    return render(request, 'contact.html', {'page_active': page_active})
+    return render(request, "pages/contact.html", {'page_active': page_active})
 
 
 def cursus(request):
     page_active = 'cursus'
-    return render(request, 'cursus.html', {'page_active': page_active})
+    return render(request, "pages/cursus.html", {'page_active': page_active})
 
 
 def filiere(request):
     page_active = 'filiere'
-    return render(request, 'filiere.html', {'page_active': page_active})
+    return render(request, "pages/filiere.html", {'page_active': page_active})
 
 
 def test(request):
     page_active = 'cursus'
-    return render(request, 'test.html', {'page_active': page_active})
+    return render(request, "pages/test.html", {'page_active': page_active})
 
 
 def creer_questionnaire(request):
@@ -44,7 +43,7 @@ def creer_questionnaire(request):
     else:
         form = QuestionnaireForm()
 
-    return render(request, 'creer_questionnaire.html', {'form': form})
+    return render(request, "pages/creer_questionnaire.html", {'form': form})
 
 
 def prediction_view(request):
@@ -56,8 +55,8 @@ def prediction_view(request):
             # Effectuez les prédictions en utilisant votre modèle
             predictions = VotreModele.predict([data['feature1'], data['feature2'], ...])
             # Envoyez les prédictions au template
-            return render(request, 'resultat.html', {'predictions': predictions})
+            return render(request, "resultat.html", {'predictions': predictions})
     else:
         form = VotreFormulaire()
 
-    return render(request, 'formulaire.html', {'form': form})
+    return render(request, "pages/formulaire.html", {'form': form})
