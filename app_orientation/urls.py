4645d136dabf23c13771a25903b857f015f6ff74
jupi-z <ameno.aton4@gmail.com>
2024-01-24 10:26:47 +0200

    integration template 60%

--- app_orientation/urls.py
+++ app_orientation/urls.py
@@ -1,15 +1,28 @@
 from django.urls import path
-from . import views
-from .views import creer_questionnaire
-from .views import prediction_view
+from app_orientation.views import (
+    AccueilView,
+    AboutView,
+    AptitudeView,
+    CompetancesView,
+    AssistanceView,
+    OrientationView,
+    ConseilsOrientationView,
+    CreerQuestionnaireView,
+    PredictionView,
+    ServicesView,
+    PricingView,
+)
 
 urlpatterns = [
-    path('', views.accueil, name='accueil'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
-    path('cursus/', views.cursus, name='cursus'),
-    path('filiere/', views.filiere, name='filiere'),
-    path('test/', views.test, name='test'),
-    path('questionnaire/', creer_questionnaire, name='creer_questionnaire'),
-    path('prediction/', prediction_view, name='prediction')
+    path('', AccueilView.as_view(), name='accueil'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('aptitude/', AptitudeView.as_view(), name='aptitude'),
+    path('competances/', CompetancesView.as_view(), name='competances'),
+    path('assistance/', AssistanceView.as_view(), name='assistance'),
+    path('orientation/', OrientationView.as_view(), name='orientation'),
+    path('conseils_orientation/', ConseilsOrientationView.as_view(), name='conseils_orientation'),
+    path('creer_questionnaire/', CreerQuestionnaireView.as_view(), name='creer_questionnaire'),
+    path('prediction/', PredictionView.as_view(), name='prediction'),
+    path('services/', ServicesView.as_view(), name='services'),
+    path('pricing/', PricingView.as_view(), name='pricing'),
 ]
--- app_orientation/views.py
+++ app_orientation/views.py
@@ -1,62 +1,82 @@
-from django.shortcuts import render
 from django.shortcuts import render, redirect
-from .forms import QuestionnaireForm
-from .forms import VotreFormulaire
+from django.views import View
+from app_orientation.forms import QuestionnaireForm, VotreFormulaire
 
 
-def accueil(request):
-    page_active = '/'
-    return render(request, 'index.html', {'page_active': page_active})
+class AccueilView(View):
+    def get(self, request):
+        page_active = '/'
+        return render(request, 'index.html', {'page_active': page_active})
 
 
-def about(request):
-    page_active = 'about'
-    return render(request, "pages/about.html", {'page_active': page_active})
+class AboutView(View):
+    def get(self, request):
+        page_active = 'about'
+        return render(request, "pages/front-pages/about.html", {'page_active': page_active})
 
 
-def contact(request):
-    page_active = 'contact'
-    return render(request, "pages/contact.html", {'page_active': page_active})
+class AptitudeView(View):
+    def get(self, request):
+        page_active = 'Tests d\'aptitude'
+        return render(request, "pages/front-pages/aptitude.html", {'page_active': page_active})
 
 
-def cursus(request):
-    page_active = 'cursus'
-    return render(request, "pages/cursus.html", {'page_active': page_active})
+class CompetancesView(View):
+    def get(self, request):
+        page_active = 'Evaluations de competances'
+        return render(request, "pages/front-pages/competances.html", {'page_active': page_active})
 
 
-def filiere(request):
-    page_active = 'filiere'
-    return render(request, "pages/filiere.html", {'page_active': page_active})
+class AssistanceView(View):
+    def get(self, request):
+        page_active = 'assistance'
+        return render(request, "pages/front-pages/assistance.html", {'page_active': page_active})
 
 
-def test(request):
-    page_active = 'cursus'
-    return render(request, "pages/test.html", {'page_active': page_active})
+class OrientationView(View):
+    def get(self, request):
+        page_active = 'Services d\'orientation'
+        return render(request, "pages/front-pages/orientation.html", {'page_active': page_active})
 
 
-def creer_questionnaire(request):
-    if request.method == 'POST':
+class ConseilsOrientationView(View):
+    def get(self, request):
+        page_active = 'Conseils d\'orientation'
+        return render(request, "pages/front-pages/Conseils_orientation.html", {'page_active': page_active})
+
+
+class ServicesView(View):
+    def get(self, request):
+        page_active = 'Services'
+        return render(request, "pages/front-pages/services.html", {'page_active': page_active})
+
+
+class PricingView(View):
+    def get(self, request):
+        page_active = 'pricing'
+        return render(request, "pages/front-pages/pricing-page.html", {'page_active': page_active})
+
+
+class CreerQuestionnaireView(View):
+    def get(self, request):
+        form = QuestionnaireForm()
+        return render(request, "pages/creer_questionnaire.html", {'form': form})
+
+    def post(self, request):
         form = QuestionnaireForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('resultats')
-    else:
-        form = QuestionnaireForm()
-
-    return render(request, "pages/creer_questionnaire.html", {'form': form})
 
 
-def prediction_view(request):
-    if request.method == 'POST':
+class PredictionView(View):
+    def get(self, request):
+        form = VotreFormulaire()
+        return render(request, "pages/formulaire.html", {'form': form})
+
+    def post(self, request):
         form = VotreFormulaire(request.POST)
         if form.is_valid():
-            # Récupérez les données du formulaire
             data = form.cleaned_data
-            # Effectuez les prédictions en utilisant votre modèle
             predictions = VotreModele.predict([data['feature1'], data['feature2'], ...])
-            # Envoyez les prédictions au template
             return render(request, "resultat.html", {'predictions': predictions})
-    else:
-        form = VotreFormulaire()
-
-    return render(request, "pages/formulaire.html", {'form': form})
